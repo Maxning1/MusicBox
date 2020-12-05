ffea7f54eb5b23473a3012d9aa25ffc3808255f8
Max Ning <maxning24@gmail.com>
2020-12-04 23:01:21 -0500

    Updated backend post response for creating rooms

--- audio_controller/audio_controller/urls.py
+++ audio_controller/audio_controller/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', include('api_handler.urls')),
+    path('api/', include('api_handler.urls')),
     path('', include('frontend.urls'))
 ]
