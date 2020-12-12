8a9aa3a4955fffa7aa038c3c1e24aed6f3fd23d0
Max Ning <maxning24@gmail.com>
2020-12-11 21:04:59 -0500

    Added Join Room page and POST functionality.

--- audio_controller/frontend/src/components/CreateRoomPage.js
+++ audio_controller/frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
+import { TextField, Button, Grid, Typography, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
   defaultVotes = 2;
