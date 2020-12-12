dc4482112b781b64d77192aba067822ae8d6973b
Max Ning <maxning24@gmail.com>
2020-12-11 22:28:56 -0500

    Updated Home Page, Fixed leave room functionality and rerouted home page for inactive rooms.

--- audio_controller/api_handler/views.py
+++ audio_controller/api_handler/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -74,3 +75,25 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+class IsUserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
--- audio_controller/frontend/src/components/HomePage.js
+++ audio_controller/frontend/src/components/HomePage.js
@@ -1,25 +1,88 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
+import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
-import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor (props){
+    constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
+  }
+
+    async componentDidMount() {
+        fetch("/api/user-in-room")
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+            roomCode: data.code,
+            });
+        });
     }
-    
-    render(){
+
+    renderHomePage() {
         return (
-            <Router>
-                <Switch>
-                    <Route exact path ="/"><p>This is the Home Page</p></Route>
-                    <Route path ="/join" component={RoomJoinPage} />
-                    <Route path ="/create" component={CreateRoomPage} />
-                    <Route path = "/room/:roomCode" component={Room}/>
-                </Switch>
-            </Router>
+        <Grid container spacing={3}>
+            <Grid item xs={12} align="center">
+            <Typography variant="h3" compact="h3">
+                House Party
+            </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+                <Button color="primary" to="/join" component={Link}>
+                Join a Room
+                </Button>
+                <Button color="secondary" to="/create" component={Link}>
+                Create a Room
+                </Button>
+            </ButtonGroup>
+            </Grid>
+        </Grid>
+        );
+    }
+
+    clearRoomCode() {
+        this.setState({
+        roomCode: null,
+        });
+    }
+
+    render() {
+        return (
+        <Router>
+            <Switch>
+            <Route
+                exact
+                path="/"
+                render={() => {
+                return this.state.roomCode ? (
+                    <Redirect to={`/room/${this.state.roomCode}`} />
+                ) : (
+                    this.renderHomePage()
+                );
+                }}
+            />
+            <Route path="/join" component={RoomJoinPage} />
+            <Route path="/create" component={CreateRoomPage} />
+            <Route
+                path="/room/:roomCode"
+                render={(props) => {
+                return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                }}
+            />
+            </Switch>
+        </Router>
         );
     }
 }
