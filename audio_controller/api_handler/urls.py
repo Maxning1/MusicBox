1e533a328c0e6a1c6e5ab5f25e1b7f9dcc5e2543
Max Ning <maxning24@gmail.com>
2020-12-20 22:01:01 -0500

    Created settings page for rooms with host-only access.

--- audio_controller/api_handler/urls.py
+++ audio_controller/api_handler/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, IsUserInRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, IsUserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', IsUserInRoom.as_view()),
     path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view()),
 ]
