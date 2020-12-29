43b007367c52d06456b6abd5698e7b81be123fb5
Max Ning <maxning24@gmail.com>
2020-12-28 21:36:45 -0500

    Added TrackPlayer component to Room page

--- audio_controller/frontend/src/components/Room.js
+++ audio_controller/frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Button, Typography } from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
+import TrackPlayer from "./TrackPlayer";
 
 export default class Room extends Component {
   constructor(props) {
@@ -11,31 +12,25 @@ export default class Room extends Component {
       isHost: false,
       showSettings: false,
       spotifyAuthenticated: false,
+      song: {},
     };
     this.roomCode = this.props.match.params.roomCode;
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
     this.updateShowSettings = this.updateShowSettings.bind(this);
-    this.renderSettings = this.renderSettings.bind(this);
     this.renderSettingsButton = this.renderSettingsButton.bind(this);
+    this.renderSettings = this.renderSettings.bind(this);
     this.getRoomDetails = this.getRoomDetails.bind(this);
-    this.getRoomDetails();
     this.authenticateSpotify = this.authenticateSpotify.bind(this);
+    this.getCurrentSong = this.getCurrentSong.bind(this);
+    this.getRoomDetails();
   }
 
-  authenticateSpotify() {
-    fetch("/spotify/is-authenticated")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ spotifyAuthenticated: data.status });
-        console.log(data.status);
-        if (!data.status) {
-          fetch("/spotify/get-auth-url")
-            .then((response) => response.json())
-            .then((data) => {
-              window.location.replace(data.url);
-            });
-        }
-      });
+  componentDidMount() {
+    this.interval = setInterval(this.getCurrentSong, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   getRoomDetails() {
@@ -54,8 +49,39 @@ export default class Room extends Component {
           isHost: data.is_host,
         });
         if (this.state.isHost) {
-          this.authenticateSpotify()
+          this.authenticateSpotify();
         }
+      });
+  }
+
+  authenticateSpotify() {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ spotifyAuthenticated: data.status });
+        console.log(data.status);
+        if (!data.status) {
+          fetch("/spotify/get-auth-url")
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.replace(data.url);
+            });
+        }
+      });
+  }
+
+  getCurrentSong() {
+    fetch("/spotify/current-song")
+      .then((response) => {
+        if (!response.ok) {
+          return {};
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.setState({ song: data });
+        console.log(data);
       });
   }
 
@@ -70,44 +96,47 @@ export default class Room extends Component {
     });
   }
 
-  updateShowSettings(value){
-    this.setState ({
+  updateShowSettings(value) {
+    this.setState({
       showSettings: value,
     });
-  }
-
-  renderSettingsButton() {
-    return (
-      <Grid item xs={12} align="center">
-        <Button variant="contained" color="primary" onClick={() => this.updateShowSettings(true)}>
-        Settings
-        </Button>
-      </Grid>
-    );
   }
 
   renderSettings() {
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <CreateRoomPage 
-          update={true} 
-          votesToSkip={ this.state.votesToSkip } 
-          guestCanPause={ this.state.guestCanPause }
-          roomCode={ this.roomCode } 
-          updateCallback={this.getRoomDetails} 
+          <CreateRoomPage
+            update={true}
+            votesToSkip={this.state.votesToSkip}
+            guestCanPause={this.state.guestCanPause}
+            roomCode={this.roomCode}
+            updateCallback={this.getRoomDetails}
           />
-
         </Grid>
         <Grid item xs={12} align="center">
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => this.updateShowSettings(false)}
-        >
-          Close
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
+          >
+            Close
           </Button>
         </Grid>
+      </Grid>
+    );
+  }
+
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
       </Grid>
     );
   }
@@ -123,21 +152,7 @@ export default class Room extends Component {
             Code: {this.roomCode}
           </Typography>
         </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Votes: {this.state.votesToSkip}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Guest Can Pause: {this.state.guestCanPause.toString()}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Host: {this.state.isHost.toString()}
-          </Typography>
-        </Grid>
+        <TrackPlayer {...this.state.song} />
         {this.state.isHost ? this.renderSettingsButton() : null}
         <Grid item xs={12} align="center">
           <Button
